cac194cbcf7c693a2ad5607cf4dd174620690409
Xuan Cuong <94371015+xuancuong244@users.noreply.github.com>
2023-08-26 14:39:59 +0700

    a
    a

---
+++ src/main/resources/static/js/my-shop-app.js
@@ -0,0 +1,87 @@
+const app = angular.module("my-shop-app", []);
+app.controller("my-shop-ctrl", function ($scope, $http) {
+    $scope.keyword = "";
+    //Quản lý giỏ hàng
+    $scope.cart = {
+        items: [],
+        //Thêm sản phẩm vào giỏ hàng (Add)
+        add(id) {
+            var item = this.items.find(item => item.id == id);
+            if (item) {
+                item.qty++;
+                this.saveToLocalStorage();
+            } else {
+                $http.get(`/api/products/${id}`).then(resp => {
+                    resp.data.qty = 1;
+                    this.items.push(resp.data);
+                    this.saveToLocalStorage();
+                })
+            }
+        },
+        //Xóa sản phẩm khỏi giỏ hàng (Remove)
+        remove(id) {
+            var index = this.items.findIndex(item => item.id == id);
+            this.items.splice(index, 1);
+            this.saveToLocalStorage();
+        },
+        //Xóa sạch giỏ hàng (Clear all)
+        clear() {
+            this.items = []
+            this.saveToLocalStorage();
+        },
+        //Tính tiền của 1 sản phẩm
+        amt_of(item) {
+        },
+        //Tính tổng số lượng các sản phẩm trong giỏ hàng
+        get count() {
+            return this.items
+                .map(item => item.qty)
+                .reduce((total, qty) => total += qty, 0);
+
+        },
+        //Tổng thành tiền tất cả sản phẩm có trong giỏ hàng
+        get amount() {
+            return this.items
+                .map(item => item.qty * item.price)
+                .reduce((total, qty) => total += qty, 0);
+        },
+        //Lưu giỏ hàng vào local storage
+        saveToLocalStorage() {
+            var json = JSON.stringify(angular.copy(this.items));
+            localStorage.setItem("cart", json);
+        },
+        //Đọc giỏ hàng từ local storage
+        loadFromLocalStorage() {
+            var json = localStorage.getItem("cart");
+            this.items = json ? JSON.parse(json) : [];
+        }
+    }
+    $scope.cart.loadFromLocalStorage();
+
+    $scope.order = {
+        createDate: new Date(),
+        address: "",
+        account: {username: $("#username").text()},
+        get orderDetails() {
+            return $scope.cart.items.map(item => {
+                return {
+                    product: {id: item.id},
+                    price: item.price,
+                    quantity: item.qty
+                }
+            });
+        },
+        purchase() {
+            var order = angular.copy(this);
+            //Thực hiện đặt hàng
+            $http.post("/api/orders", order).then(resp => {
+                alert("Đặt hàng thành công!");
+                $scope.cart.clear();
+                location.href = "/order/detail/" + resp.data.id;
+            }).catch(error => {
+                alert("Đặt hàng lỗi!")
+                console.log(error)
+            })
+        }
+    }
+})
